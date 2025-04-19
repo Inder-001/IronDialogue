31883e7d1439b9bb177d2e03268700d3d0226a2e
Onepard <onepard@Rajats-Mac-mini.local>
2025-04-19 13:01:50 +0530

    app name

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,13 @@ function App() {
 	return (
 		<>
 			<div>
-				<h1> My App</h1>
+				<h1>My App</h1>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
